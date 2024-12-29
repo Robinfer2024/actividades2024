0d23f4c63fc008e14d0e5261c3afbd46907e1168
Robinfer2024 <rofeheca@gmail.com>
2024-12-29 15:50:03 -0500

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,16 @@
+﻿function displayActivities() {
+    const activities = [
+        { time: "07:00", activity: "Correr en el parque" },
+        { time: "09:00", activity: "Clase de física" },
+        { time: "12:30", activity: "Almuerzo con amigos" },
+        { time: "14:00", activity: "Revisión de tareas" },
+        { time: "19:00", activity: "Ver una película" },
+    ];
+
+    const activityList = document.getElementById("activityList");
+    activities.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = `${item.time} - ${item.activity}`;
+        activityList.appendChild(li);
+    });
+}
